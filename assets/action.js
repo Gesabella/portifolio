e73765c5f86c218626c432d38e9f9cc0f549535f
Gesabella <gesabellamonteiro@gmail.com>
2026-01-23 23:09:46 -0300

    ajuste paradispositivosmenoresque 400px e fechameno do menu hamburguer após clique em item

--- assets/action.js
+++ assets/action.js
@@ -1,4 +1,14 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
+const links = document.querySelectorAll(".nav a");
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+hamburguer.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
